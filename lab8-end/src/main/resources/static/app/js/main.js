bfb8bf4ed2be9f73e85e5fc1a098670ea15ba2f1
nenadtodEdu <n.todor@outlook.com>
2018-01-08 21:03:41 +0100

    dodat materijal sa casa

---
+++ lab8-end/src/main/resources/static/app/js/main.js
@@ -0,0 +1,177 @@
+var wafepa = angular.module("wafepaApp", ['ngRoute']);
+
+wafepa.controller("activitiesCtrl", function ($scope, $http, $location){
+	
+	var baseUrl = "/api/activities";
+	
+	$scope.activities = [];
+	
+	$scope.newActivity = {};
+	$scope.newActivity.name = "";
+	
+	$scope.gotActivities = false;
+	
+	var getActivities = function(){
+	
+		var promise = $http.get(baseUrl);
+		promise.then(
+			function success(answ){
+				$scope.gotActivities = true;
+				$scope.activities = answ.data;
+			},
+			function error(answ){
+				alert("Something went wrong");
+				console.log(answ);
+			}
+		);
+	}
+	
+	getActivities();
+	
+	$scope.addActivity = function (){
+		var promise = $http.post(baseUrl, $scope.newActivity);
+		promise.then(
+			function uspeh(odg){
+				getActivities();
+				$scope.newActivity.name = "";
+			},
+			function neuspeh(odg){
+				alert("Something went wrong!");
+				console.log(odg);
+			}
+		);
+	}
+	
+	$scope.goToEdit = function(id){
+		$location.path('/activities/edit/' + id)		
+	}
+	
+	$scope.deleteActivity = function (id){
+		$http.delete(baseUrl + "/" + id).then(
+			function success(answ){
+				getActivities();
+			},
+			function error(answ){
+				alert("Couldn't delete activity!");
+			}	
+		);
+	}
+	
+});
+
+wafepa.controller("editActivityCtrl", function($scope, $routeParams, $http, $location){
+	
+	var baseUrl = "/api/activities";
+	var aid = $routeParams.aid;
+	
+	$scope.oldActivity = {};
+	$scope.oldActivity.name = "";
+	
+	var getActivity = function(){
+		
+		$http.get(baseUrl + "/" + aid).then(
+			function success(answ){
+				$scope.oldActivity = answ.data;
+			},
+			function error(answ){
+				alert('Something went wrong!');
+			}
+		);		
+	}
+	
+	getActivity();
+	
+	
+	$scope.edit = function(){
+		
+		$http.put(baseUrl + "/" + aid, $scope.oldActivity).then(
+			function success(answ){
+				$location.path("/activities");
+			},
+			function error(answ){
+				alert('Something went wrong!');
+			}
+		);
+	}
+	
+});
+
+wafepa.controller("standoviCtrl", function($scope, $http){
+	
+	var baseUrl = "/api/standovi";
+	var baseUrlSajam = "/api/sajmovi";
+	
+	$scope.standovi = [];
+	$scope.sajmovi = [];
+	
+	$scope.noviStand = {};
+	$scope.noviStand.zakupac = "";
+	$scope.noviStand.povrsina = "";
+	$scope.noviStand.sajamId = 0;
+	
+	var getStandovi = function(){
+		
+		var promise = $http.get(baseUrl);
+		promise.then(
+			function uspeh(odg){
+				$scope.standovi = odg.data;
+			},
+			function neuspeh(odg){
+				alert("Something went wrong!");
+			}
+		);
+	}
+	
+	var getSajmovi = function(){
+		
+		var promise = $http.get(baseUrlSajam);
+		promise.then(
+			function uspeh(odg){
+				$scope.sajmovi = odg.data;
+			},
+			function neuspeh(odg){
+				alert("Couldn't fetch sajam's!");
+			}
+		)
+		
+	}
+	
+	getStandovi();
+	getSajmovi();
+	
+	$scope.addStand = function(){
+		
+		var promise = $http.post(baseUrl, $scope.noviStand);
+		promise.then(
+			function success(answ){
+				getStandovi();
+			},
+			function error(answ){
+				alert("Something went wrong!");
+			}
+		);
+	}
+	
+	$scope.changeHappened = function(){
+		alert("Hello!");
+	}
+});
+
+wafepa.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '/app/partial/home.html'
+		})
+		.when('/activities', {
+			templateUrl : '/app/partial/activities.html'
+		})
+		.when('/activities/edit/:aid', {
+			templateUrl : '/app/partial/edit_activity.html'
+		})
+		.when('/standovi', {
+			templateUrl : '/app/partial/standovi.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
